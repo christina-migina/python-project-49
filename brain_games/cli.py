f748990e487dec98a31113738d58ebadd8b46b32
Christina <christina.migina@abv.bg>
2025-09-27 15:57:23 +0300

    add function welcome_user

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,3 +1,7 @@
 import prompt
 
-name = prompt.string("May I have your name? ")
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
