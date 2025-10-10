c2ec1f8e9760b945d6fef09594747b7ff5b7004f
Christina <christina.migina@abv.bg>
2025-10-10 23:37:08 +0300

    fixed brain_even.py, add brain-even script entry, add asciinema demo

---
+++ READ.md
@@ -0,0 +1,2 @@
+[![asciicast](https://asciinema.org/a/dqlc47TG45Mjvhlnt4l69RJHl.svg)](https://asciinema.org/a/dqlc47TG45Mjvhlnt4l69RJHl)
+
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,36 +2,30 @@ from brain_games.cli import welcome_user
 from random import randint
 
 
-# функция для генерация числа, вопроса и ответа, проверки числа на четность функцией
-def ask_question():
+def get_answer():
     number = randint(1, 100)
     print(f"Question: {number}")
-    return input("Your answer: "), is_even(number)
+    player_answer = input("Your answer: ")
+    correct_answer = "yes" if is_even(number) else "no"
+    return player_answer, correct_answer
 
 
-# функция для проверки числа на четность
 def is_even(number):
-    if number % 2 == 0:
-        correct_answer = "yes"
-    else:
-        correct_answer = "no"
-    return correct_answer
+    return number % 2 == 0
 
 
-# функция для проверки верности ответа
-def check_answer(player_answer, correct_answer):
+def is_answer_correct(player_answer, correct_answer):
     return player_answer == correct_answer
 
 
-# игра
 def even_game():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     counter = 0
 
     while counter < 3:
-        player_answer, correct_answer = ask_question()
-        if check_answer(player_answer, correct_answer):
+        player_answer, correct_answer = get_answer()
+        if is_answer_correct(player_answer, correct_answer):
             print("Correct!")
             counter += 1
         else:
